3641d410fccbfe5736ec0990d94240526deb173c
Marcio Fraga <marcior.mfraga@gmail.com>
2021-05-01 16:51:18 -0300

    first commit

---
+++ src/main/java/com/foursales/challengeFS/ChallengeFsApplication.java
@@ -0,0 +1,13 @@
+package com.foursales.challengeFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeFsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foursales/challengeFS/ChallengeFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foursales.challengeFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
